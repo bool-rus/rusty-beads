5a9f00b06980ca3488a5bb8c6c595ed9e73dd94d
Баринов Сергей Владимирович <SVBarinov.SBT@sberbank.ru>
2020-06-19 11:34:42 +0300

    just more colors in palette

--- src/ui/palette.rs
+++ src/ui/palette.rs
@@ -44,13 +44,30 @@ impl AppWidget for Palette {
 
 impl Default for Palette {
     fn default() -> Self {
-        Palette::new(
-            vec![
-                Color {r: 0, g:0, b:0},
-                Color {r: 255, g:255, b:255},
-                Color {r: 200, g:0, b:0},
-                Color {r: 0, g:200, b:0},
-                Color {r: 0, g:0, b:200},
-            ])
+        Palette::new(vec![
+            Color { r: 0x61 ,   g: 0x00,    b: 0x00 },
+            Color { r: 0x98,    g: 0x02,    b: 0x2f },
+            Color { r: 0xcf,    g: 0x32,    b: 0x00 },
+            Color { r: 0xff,    g: 0x32,    b: 0x32 },
+            Color { r: 0xfd,    g: 0x8c,    b: 0x0e },
+            Color { r: 0xff,    g: 0xe6,    b: 0x11 },
+            Color { r: 0xff,    g: 0xfc,    b: 0x72 },
+            Color { r: 0x88,    g: 0x0c,    b: 0x14 },
+            Color { r: 0xb0,    g: 0x5e,    b: 0x07 },
+            Color { r: 0x29,    g: 0x13,    b: 0x9c },
+            Color { r: 0x3f,    g: 0x9b,    b: 0xe3 },
+            Color { r: 0x69,    g: 0xc7,    b: 0xac },
+            Color { r: 0x9a,    g: 0xcc,    b: 0xb0 },
+            Color { r: 0x49,    g: 0x8c,    b: 0x55 },
+            Color { r: 0x00,    g: 0xb1,    b: 0x5a },
+            Color { r: 0x3e,    g: 0xe0,    b: 0x19 },
+            Color { r: 0x8d,    g: 0xe4,    b: 0x6f },
+            Color { r: 0x8c,    g: 0x62,    b: 0xd3 },
+            Color { r: 0xc8,    g: 0xb5,    b: 0xff },
+            Color { r: 0xff,    g: 0xff,    b: 0xff },
+            Color { r: 0xdd,    g: 0xdd,    b: 0xdd },
+            Color { r: 0x90,    g: 0x93,    b: 0x9e },
+            Color { r: 0x00,    g: 0x00,    b: 0x00 },
+        ])
     }
 }
