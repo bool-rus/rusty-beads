64ca3606014f0185f77d32fdc2ef3dc3c6776066
Баринов Сергей Владимирович <SVBarinov.SBT@sberbank.ru>
2020-07-07 23:25:54 +0300

    #36 undo/redo functionality of grid::Service

--- src/service/grid.rs
+++ src/service/grid.rs
@@ -1,14 +1,17 @@
 use crate::entities::*;
 use crate::grid::Grid;
 use std::rc::Rc;
-use std::num::NonZeroU32;
 use std::fmt::Debug;
-use std::thread::current;
+use std::collections::VecDeque;
+use std::num::NonZeroUsize;
+use core::mem;
 
 #[derive(Debug, Clone)]
 pub enum Message<T: Debug + Clone> {
     Ignore,
-    Point(Coord),
+    Undo,
+    Redo,
+    Point(Coord, T),
     Grow(Side),
     Shrink(Side),
     Resize(Size),
@@ -24,49 +27,132 @@ impl<T: Debug + Clone> From<Result<Message<T>, String>> for Message<T> {
         }
     }
 }
-
+#[derive(Default)]
 pub struct Service<T: Debug + Clone> {
     grid: Grid<T>,
-    current: T,
+    undo: Vec<Message<T>>,
+    redo: Vec<Message<T>>,
 }
 
 impl<T: Debug + Clone> Service<T> {
     fn updated(&self) -> Message<T> {
         Message::Updated(Rc::new(self.grid.clone()))
     }
+    fn push_undo(&mut self, msg: Message<T>) {
+        self.undo.push(msg);
+        self.redo.clear();
+    }
 }
 
-impl<T: Default + Debug + Clone> super::Service for Service<T> {
+impl<T: Default + Debug + Clone + PartialEq> super::Service for Service<T> {
     type Message = Message<T>;
 
     fn service(&mut self, msg: Self::Message) -> Option<Self::Message> {
         use Message::*;
-        //TODO: implement undo logic
         match msg {
-            Point(Coord{x,y}) => {
+            Point(Coord{x,y}, new) => {
                 let msg = self.grid
-                    .set(x,y, self.current.clone())
+                    .set(x,y, new.clone())
                     .map(|prev| {
+                        if new != prev {
+                            self.push_undo(Point(Coord{x,y},prev));
+                        }
                         self.updated()
                     });
                 Some(msg.into())
             },
             Grow(side) => {
                 self.grid.grow(side, Default::default());
+                self.push_undo(Shrink(side));
                 Some(self.updated())
             },
             Shrink(side) => {
                 let msg = self.grid.shrink(side)
                     .map(|_| {
+                        self.push_undo(Grow(side));
                         self.updated()
                     });
                 Some(msg.into())
             },
             Resize(Size { width, height }) => {
+                let prev = Size {
+                    width: NonZeroUsize::new(self.grid.width()).unwrap(),
+                    height: NonZeroUsize::new(self.grid.height()).unwrap(),
+                };
+                self.push_undo(Resize(prev));
                 self.grid.resize(width, height);
                 Some(self.updated())
             },
+            Undo => {
+                let mut undo = Vec::new();
+                mem::swap(&mut self.undo, &mut undo);
+                mem::swap(&mut self.undo, &mut self.redo);
+                let result = match undo.pop() {
+                    None => Some(Message::Err("Undo is empty".to_string())),
+                    Some(msg) => self.service(msg),
+                };
+                mem::swap(&mut self.undo, &mut self.redo);
+                mem::swap(&mut self.undo, &mut undo);
+                result
+            },
+            Redo => {
+                let mut redo = Vec::new();
+                mem::swap(&mut self.redo, &mut redo);
+                mem::swap(&mut self.undo, &mut self.redo);
+                let result = match redo.pop() {
+                    None => Some(Message::Err("Redo is empty".to_string())),
+                    Some(msg) => self.service(msg),
+                };
+                mem::swap(&mut self.undo, &mut self.redo);
+                mem::swap(&mut self.redo, &mut redo);
+                result
+            },
             Updated(_)| Err(_) | Ignore => None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::service::Service as _;
+
+    fn make() -> Service<u8> {
+        let mut s = Service::default();
+        for _ in 1..10 {
+            s.service(Message::Grow(Side::Top));
+            s.service(Message::Grow(Side::Left));
+        }
+        s
+    }
+
+    #[test]
+    fn test_undo() {
+        let mut s = make();
+        s.service(Message::Point(Coord{ x: 0, y: 0 }, 33));
+        s.service(Message::Point(Coord{ x: 0, y: 0 }, 34));
+        s.service(Message::Point(Coord{ x: 0, y: 0 }, 34));
+        s.service(Message::Point(Coord{ x: 0, y: 0 }, 35));
+        let vars: Vec<_> = vec![Message::Undo; 2].into_iter().map(|m|{
+           match s.service(m).expect("undo must return message") {
+               Message::Updated(grid) => {grid.as_table()[0][0]},
+               _ => {panic!("undo must return updated")},
+           }
+        }).collect();
+        assert_eq!(vars, vec![34,33]);
+        let vars: Vec<_> = vec![Message::Redo; 2].into_iter().map(|m|{
+            match s.service(m).expect("redo must return message") {
+                Message::Updated(grid) => {grid.as_table()[0][0]},
+                _ => {panic!("undo must return updated")},
+            }
+        }).collect();
+        assert_eq!(vars, vec![34,35]);
+        s.service(Message::Undo);
+        s.service(Message::Grow(Side::Left));
+        let response = s.service(Message::Redo);
+        match response {
+            Some(Message::Err(_)) => {},
+            _ => {panic!(format!("unexpected response: {:?}", response))},
         }
     }
 }
