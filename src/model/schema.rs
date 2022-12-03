a4104b70ff0d0613d8fd620235df43b20277d364
Serge Barinov <bool.rus@gmail.com>
2022-12-03 13:42:20 +0300

    делаем расчет линии для схемы без смещения

--- src/model/schema.rs
+++ src/model/schema.rs
@@ -1,5 +1,5 @@
 use serde::{Serialize, Deserialize};
-
+use super::Coord;
 
 #[derive(Serialize, Deserialize)]
 enum SchemaOld {
@@ -51,10 +51,162 @@ impl Schema {
     pub fn base(&self) -> usize {
         self.base_offset
     }
+    pub fn make_line(&self, start: Coord, end: Coord, width: usize) -> Vec<Coord> {
+        match self {
+            Self {base_offset: 1, offset_step: 0} => make_line(start, end, width),
+            _ => vec![end],
+        }
+    }
 }
 
 impl Default for Schema {
     fn default() -> Self {
         Self {base_offset: 2, offset_step: 1}
+    }
+}
+
+
+fn normalize_x(dist: &mut f32, width: usize) {
+    let width = width as f32;
+    if 2.0*dist.abs() > width {
+        if *dist > 0.0 {
+            *dist -= width;
+        } else {
+            *dist += width;
+        }
+    }
+}
+
+fn make_line(start: Coord, end: Coord, width: usize) -> Vec<Coord> {
+    if start == end {
+        return vec![start];
+    }
+    use std::mem::swap;
+    let egui::Vec2 {mut x, mut y} = end - start;
+    normalize_x(&mut x, width);
+    //теперь мы будем строить функцию y = a * x
+    //но в качестве x нам нужна самая большая координата дистанции
+    let transposition = y.abs() > x.abs(); 
+    if transposition { // если y > x, значит, меняем их местами (потом поменяем обратно)
+        swap(&mut x, &mut y);
+    }
+    let a = y/x;
+    let n = x.abs();
+    let startx = (start.x + width) as f32; //добавляем ширину, чтобы не перейти через 0
+    let starty = start.y as f32;
+    let mut result = Vec::with_capacity(n.abs() as usize);
+    let mut i = 0.0f32;
+    let step = if x > 0.0 { 1.0 } else { -1.0 };
+    while i.abs() <= n.abs() {
+        let mut x = i;
+        let mut y = a * x;
+        if transposition {
+            swap(&mut x, &mut y);
+        }
+        let y = (y + starty).round() as usize;
+        let x = (x + startx).round() as usize % width;
+        let coord = Coord{x,y};
+        result.push(coord);
+        i += step;
+    }
+    result
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn assert_line(a: (usize, usize), b: (usize, usize), line: Vec<(usize,usize)>, width: usize) {
+        let (x,y) = a;
+        let a = Coord {x, y};
+        let ib = a;
+        let (x,y) = b;
+        let b = Coord{x,y};
+        let ia = b;
+        let line: Vec<_> = line.into_iter().map(|(x,y)|Coord{x,y}).collect();
+        let iline: Vec<_> = line.iter().rev().copied().collect();
+        assert_eq!(line, make_line(a, b, width));
+        assert_eq!(iline, make_line(ia, ib, width));
+    }
+
+    #[test]
+    fn line_0() {
+        let width = 100;
+        let a = (94,16);
+        let b = (92,16);
+        let line = vec![(94,16),(93,16),(92,16)];
+        assert_line(a, b, line, width);
+    }
+    #[test]
+    fn line_90() {
+        let width = 100;
+        let a = (91,12);
+        let b = (91,14);
+        let line = vec![(91,12), (91,13), (91,14)];
+        assert_line(a, b, line, width);
+    }
+    #[test]
+    fn line_45() {
+        let width = 100;
+        let a = (95,14);
+        let b = (93,12);
+        let line = vec![(95,14), (94,13), (93,12)];
+        assert_line(a, b, line, width);
+    }    
+    #[test]
+    fn line_135() {
+        let width = 100;
+        let a = (89,12);
+        let b = (87,14);
+        let line = vec![(89,12), (88,13), (87,14)];
+        assert_line(a, b, line, width);
+    }
+    #[test]
+    fn line_26() {
+        let width = 100;
+        let a = (92,19);
+        let b = (89,18);
+        let line = vec![(92,19),(91,19),(90,18),(89,18)];
+        assert_line(a, b, line, width);
+    }
+    #[test]
+    fn line_154() {
+        let width = 100;
+        let a = (97,18);
+        let b = (94,19);
+        let line = vec![(97,18),(96,18),(95,19),(94,19)];
+        assert_line(a, b, line, width);
+    }
+    #[test]
+    fn line_37() {
+        let width = 100;
+        let a = (95,28);
+        let b = (92,26);
+        let line = vec![(95,28),(94,27),(93,27),(92,26)];
+        assert_line(a, b, line, width);
+    }    
+    #[test]
+    fn line_143() {
+        let width = 100;
+        let a = (95,22);
+        let b = (92,24);
+        let line = vec![(95,22),(94,23),(93,23),(92,24)];
+        assert_line(a, b, line, width);
+    }
+    #[test]
+    fn line_53() {
+        let width = 100;
+        let a = (90,24);
+        let b = (88,21);
+        let line = vec![(90,24),(89,23),(89,22),(88,21)];
+        assert_line(a, b, line, width);
+    }
+    #[test]
+    fn line_127() {
+        let width = 100;
+        let a = (86,22);
+        let b = (84,25);
+        let line = vec![(86,22),(85,23),(85,24),(84,25)];
+        assert_line(a, b, line, width);
     }
 }
